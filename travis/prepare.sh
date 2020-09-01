a07002983555d0bacd396fcfd73d6fb70ccd1214
Dylan T <odigiman@gmail.com>
2020-09-01 21:07:40 +0100

    fixing travis properly (i hope?)

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -2,15 +2,18 @@
 
 if [ ! -d leveldb-$LEVELDB_VERSION ] || [ ! -f leveldb-$LEVELDB_VERSION/libleveldb.so ]; then
 	rm -rf leveldb-$LEVELDB_VERSION 2>/dev/null
-	curl -fsSL https://github.com/pmmp/leveldb/archive/v$LEVELDB_VERSION.tar.gz -o leveldb-$LEVELDB_VERSION.tar.gz
+	curl -fsSL https://github.com/pmmp/leveldb/archive/$LEVELDB_VERSION.tar.gz -o leveldb-$LEVELDB_VERSION.tar.gz
 	tar zxf leveldb-$LEVELDB_VERSION.tar.gz
-	cd leveldb-$LEVELDB_VERSION && OPT=-g2 make -j4
-	if [ "$SHLIB_DIR" != "" ]; then
-		cp $SHLIB_DIR/libleveldb.* .
-	fi
+	cd leveldb-$LEVELDB_VERSION
+	cmake . -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLEVELDB_BUILD_TESTS=OFF -DLEVELDB_BUILD_BENCHMARKS=OFF
+	make -j8
 	cd ..
 else
 	echo "Using cached libleveldb build"
 fi
 
-phpize && ./configure --with-leveldb=$PWD/leveldb-$LEVELDB_VERSION && make -j4
+cd leveldb-$LEVELDB_VERSION
+make install
+cd ..
+
+phpize && ./configure --with-leveldb=/usr && make -j4
